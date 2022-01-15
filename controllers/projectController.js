9cebf2e5ebbcfd5fd09814f0098a84ebfd270033
JorgeCarlotto <jorgemcarlotto@gmail.com>
2022-01-15 06:56:05 -0300

    controller config OK

---
+++ controllers/projectController.js
@@ -0,0 +1,7 @@
+exports.projectsHome = (req, res) => {
+    res.send('index page')
+}
+
+exports.aboutUs = (req, res) =>{
+    res.send("Sobre Nosotros Page");
+};
--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+//Controllers
+const projectController = require('../controllers/projectController')
+
+
+//Routers
 module.exports = function(){
-
-    router.get('/', (req, res) =>{
-        res.send("index Page");
-    });
-
-    router.get('/nosotros', (req, res) =>{
-        res.send("Sobre Nosotros Page");
-    });
-
+    router.get('/', projectController.projectsHome);
+    router.get('/nosotros',projectController.aboutUs);
+    
     return router; 
 }
